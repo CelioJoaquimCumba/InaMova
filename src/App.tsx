c6fc962bed220a39061bb9aad8a0b9defd60ee3f
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-12-18 11:12:46 +0200

    refactor: don't ignore just one file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, Pressable, StyleSheet, Text, View } from 'react-native';
 import { NativeWindStyleSheet } from "nativewind";
-import { styled, useColorScheme } from 'nativewind';
-
+import { styled, useColorScheme } from 'nativewind'
 NativeWindStyleSheet.setOutput({
   default: "native",
 });
@@ -15,7 +14,7 @@ export default function App() {
   return (
     <View className="bg-white dark:bg-black w-full h-full flex justify-center items-center space-y-4">
       <Text className="text-black dark:text-white">Open up App.js to start working on your app!</Text>
-      <Pressable className="bg-black dark:bg-white p-4 rounded-md" onPress={handleClick}><Text className='text-white dark:text-black'>{colorScheme === "light" ? "change to dark mode" : "change to light mode"}</Text></Pressable>
+      <Pressable className="bg-black dark:bg-white p-4 rounded-md" onPress={handleClick}><Text className='text-white dark:text-black'>{colorScheme === "light" ? "Change to dark mode" : "Change to light mode"}</Text></Pressable>
     </View>
   );
 }
