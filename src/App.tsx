4daf0966cbd495eedf108204724c999da82932a1
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-12-18 12:05:40 +0200

    refactor: fix import positioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { NativeWindStyleSheet } from "nativewind";
-import { styled, useColorScheme } from 'nativewind'import { Intro } from './pages/intro';
+import { styled, useColorScheme } from 'nativewind'
+import { Intro } from './pages/intro';
 
 NativeWindStyleSheet.setOutput({
   default: "native",
