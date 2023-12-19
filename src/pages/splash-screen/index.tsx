742635d27067be4f66abaa734dd18542bacecb13
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-12-19 10:43:09 +0200

    refactor: add skip button

--- src/pages/splash-screen/index.tsx
+++ src/pages/splash-screen/index.tsx
@@ -1,11 +1,19 @@
-import { Text, View } from "react-native"
+import { Pressable, Text, View } from "react-native"
 import Carousel from "../../components/organisms/Carrousel"
 import { FirstSlide } from "./first-slide"
 import { SecondSlide } from "./second-slide"
 import { ThirdSlide } from "./third-slide"
+import { Button } from "../../components/atoms/Button"
+import  Icon from  "react-native-vector-icons/Feather"
 export const SplashScreen = () => {
     return(
-        <View className="flex w-full h-full justify-center py-4">
+        <View className="flex w-full h-full justify-center py-8">
+            <View className="flex flex-row justify-end px-4">
+                <Button variant={"link"} className="w-16 ">
+                    <Text className="text-teal-800 underline">Skip</Text>
+                    <Icon name="chevron-up" size={20} color={"teal"}/>
+                </Button>
+            </View>
             <Carousel items={[<FirstSlide/>, <SecondSlide/>, <ThirdSlide/>]}/>
         </View>
     )
