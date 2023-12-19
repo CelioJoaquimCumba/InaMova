f6ce31a400878d9b79414453466b1215c6205d18
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-12-19 10:43:22 +0200

    change app.tsx position

--- App.tsx
+++ App.tsx
@@ -2,10 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { NativeWindStyleSheet } from "nativewind";
 import { styled, useColorScheme } from 'nativewind'
-import { Intro } from './pages/intro';
-import { ChipsContainer } from './components/molecules/chips-container';
-import Carousel from './components/organisms/Carrousel';
-import { SplashScreen } from './pages/splash-screen';
+import { SplashScreen } from './src/pages/splash-screen';
 
 NativeWindStyleSheet.setOutput({
   default: "native",
--- src/pages/splash-screen/index.tsx
+++ src/pages/splash-screen/index.tsx
@@ -4,14 +4,14 @@ import { FirstSlide } from "./first-slide"
 import { SecondSlide } from "./second-slide"
 import { ThirdSlide } from "./third-slide"
 import { Button } from "../../components/atoms/Button"
-import  Icon from  "react-native-vector-icons/Feather"
+// import  Icon from  "react-native-vector-icons/Feather"
 export const SplashScreen = () => {
     return(
         <View className="flex w-full h-full justify-center py-8">
             <View className="flex flex-row justify-end px-4">
                 <Button variant={"link"} className="w-16 ">
                     <Text className="text-teal-800 underline">Skip</Text>
-                    <Icon name="chevron-up" size={20} color={"teal"}/>
+                    {/* <Icon name="chevron-up" size={20} color={"teal"}/> */}
                 </Button>
             </View>
             <Carousel items={[<FirstSlide/>, <SecondSlide/>, <ThirdSlide/>]}/>
