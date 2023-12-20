825d5afac8be8d608575f44bc4f2a4a9624e91fc
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-12-20 15:50:28 +0200

    feat: make register app and fix the login file

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -4,7 +4,7 @@ import logo from "../../../assets/logo-primary.png"
 import { Button } from "../../components/atoms/Button"
 import { Input } from "../../components/atoms/Input"
 
-export const Login = () => {
+export const Register = () => {
     return(
         <View className="flex p-8 w-full h-full justify-center items-center">
             {/* header */}
@@ -16,27 +16,22 @@ export const Login = () => {
 
                 <Button variant={"outline"}><Text className="text-black">Skip</Text></Button>
             </View>
-            {/* main */}
             <View className="flex flex-col w-full flex-grow justify-center items-center space-y-2">
                 <Text className="text-teal-900 text-lg leading-7 font-bold w-full">
-                    Login to account
+                    Create an account
                 </Text>
                 <Text className="text-teal-900 text-base leading-6 font-normal">
-                    Fill with your credentials for us to start where we left.
+                    Be able to store your score, your progress to help us help you.
                 </Text>
                 <Input type="email" label="Email" placeholder="celio.joaquim.cumba@gmail.com" isInvalid={false} hint="Please enter a valid email"/>
+                <Input type="number" label="Phone Number" placeholder="849383897" isInvalid={false} hint="Please enter a valid phone number"/>
                 <Input type="password" label="Password" placeholder="********" isInvalid={false} hint="Please enter a valid password"/>
-                <View className="flex w-full flex-row justify-end ">
-                    <Text className="text-sm leading-6 font-medium text-teal-900 underline">
-                        Forgot Password?
-                    </Text>
-                </View>
                 <Button className="w-full">
-                    <Text className="text-white">Login</Text>
+                    <Text className="text-white">Register</Text>
                 </Button>
                 <View className="flex flex-row w-full space-x-2 justify-center items-center ">
-                    <Text className="text-gray-500 text-sm leading-5 font-medium ">Don’t have an account?</Text>
-                    <Text className="text-teal-900 text-sm leading-5 font-medium underline">Register</Text>
+                    <Text className="text-gray-500 text-sm leading-5 font-medium "> Already have an account?</Text>
+                    <Text className="text-teal-900 text-sm leading-5 font-medium underline">Login</Text>
                 </View>
 
             </View>
