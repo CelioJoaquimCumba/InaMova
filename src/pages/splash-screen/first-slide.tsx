6c67f0b10e3a1466be598dd20d9730587106fd8a
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-12-20 13:33:11 +0200

    refactor: fix typo

--- src/pages/splash-screen/first-slide.tsx
+++ src/pages/splash-screen/first-slide.tsx
@@ -7,7 +7,7 @@ export const FirstSlide = () => {
         <View className="h-full flex flex-col justify-center items-center px-8 space-y-4">
             <Image source={CarDriving} className="w-64 h-64" />
             <Text className="text-xl leading-7 font-bold text-center text-teal-700">Welcome to InaMova</Text>
-            <Text className="text-base leading-6 font-normal text-gray-600 text-center">Our main mission is to help you get your driving license  as soon as possible, so that you can deslocate yourself wih more ease, making sure that you have the knowladge to.</Text>
+            <Text className="text-base leading-6 font-normal text-gray-600 text-center">Our main mission is to help you get your driving license  as soon as possible, so that you can dislocate yourself wih more ease, making sure that you have the knowledge to.</Text>
         </View>
     )
 }
