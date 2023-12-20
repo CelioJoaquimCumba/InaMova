a63d56452e40b616f03c33761ec99412c976abe2
snitraM4500 <martins.duamba@isutc.ac.mz>
2023-12-20 23:29:35 +0200

    Working on Main

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -3,8 +3,6 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { Pressable, Text } from "react-native"
 import { cn } from "../../lib/utils"
 
-
-
 const buttonVariants = cva(
   "inline-flex flex-row items-center justify-center space-x-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -18,46 +18,59 @@ const Main = () => {
           <Text>Placeholder</Text>
         </View>
       </View>
-      <View className="w-full flex flex-row items-center justify-center mt-2 h-1/6 ">
-        <View className="w-4/6 flex flex-row justify-center items-center">
-          <View className="w-1/2 h-full">
-            <Image source={images.GirlStudying} className="w-28 h-28" />
+      <View className="w-full flex flex-row items-center justify-center mt-2 h-1/6  ">
+        <View className="w-5/6 flex flex-row justify-center items-center shadow-2xl">
+          <View className="w-1/2 h-full ">
+            <Image source={images.GirlStudying} className="w-38 h-28" />
           </View>
           <View className="w-40  h-full flex flex-col-reverse">
-            <View className="flex flex-row-reverse">
-              <Button>
+            <View className="flex flex-row-reverse mr-2 ">
+              <Button className="rounded-full shadow-xl " size={"icon"}>
                 <Image source={icons.arrowright} className="h-4 w-4" />
                 {/* <Text className="">Ola</Text> */}
               </Button>
             </View>
-            <Text className="text-left text-base font-normal bg-black">
+            <Text className="text-left text-base font-normal ">
               Daily questions to prepare for the exam
             </Text>
           </View>
         </View>
       </View>
-      <View className="w-full flex flex-column items-center mt-2 h-auto">
-        <View className="w-4/6 bg-primaryIM h-16">a</View>
+      <View className="w-full flex flex-column items-center mt-2 h-auto ">
+        <View className="w-5/6 bg-primaryIM h-18 border-2 border-teal-900 bg-teal-200 rounded-2xl flex flex-row">
+          <View className="basis-4/6 m-1 mb-2 ">
+            <Text className="text-teal-900 font-bold">Get Premium</Text>
+            <Text className="text-teal-900 ">
+              Get access to all lectures, tests and the highway code
+            </Text>
+          </View>
+          <View className="basis-2/6 justify-center items-center  pr-3">
+            <Button className="rounded-lg">
+              <Text className="text-white">Purchase</Text>
+            </Button>
+          </View>
+        </View>
       </View>
-      <View className="w-full flex flex-column items-center mt-2 h-auto">
-        <View className="w-full flex px-16">
+      <View className="w-full flex flex-column items-center mt-2 h-auto ">
+        <View className="w-5/6">
           <Text>Your practice tests</Text>
         </View>
         <View className="w-4/6 h-auto flex flex-row justify-between ">
-          <View className="w-1/2 bg-primaryIM m-1 h-20"></View>
-          <View className="w-1/2 bg-primaryIM m-1 h-20"></View>
+          <View className="basis-3/6  border-2 border-black mr-2 ">
+            {/* <Image source={}/> */}
+            <Text>25 free questions</Text>
+            <Button className="border-2 border-gray-500" variant={"outline"}><Text className="text-black ">Start</Text></Button>
+          </View>
+          <View className="basis-3/6 border-black border-2 pl-2 h-40 ml-2"></View>
         </View>
       </View>
-      <View className="w-full flex flex-column items-center mt-2 h-2/6">
-        <View className="w-full flex px-16">
+      <View className="w-full flex flex-column items-center mt-2 h-auto ">
+        <View className="w-5/6">
           <Text>Your progress</Text>
         </View>
-        <View className="w-4/6 h-auto flex flex-row justify-between ">
-          <View className="w-full bg-primaryIM h-14"></View>
-        </View>
-        <View className="w-4/6 h-auto flex flex-row justify-between ">
-          <View className="w-1/2 bg-primaryIM m-1 h-20"></View>
-          <View className="w-1/2 bg-primaryIM m-1 h-20"></View>
+        <View className="w-5/6 h-auto flex flex-row justify-between ">
+          <View className="basis-2/6 bg-teal-900"></View>
+          <View className="basis-2/6 bg-teal-900"></View>
         </View>
       </View>
       <Footer />
