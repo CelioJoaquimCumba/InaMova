3c3e0eb77c8dc210fcaa6ae07f0c944ed606a400
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-12-22 12:35:43 +0200

    refactor: add padding bottom to tab navigator

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -12,7 +12,7 @@ const Tab = createBottomTabNavigator()
 export const Main = () => {
     return(
         <View className="flex w-full h-full">
-            <Tab.Navigator initialRouteName="Test" screenOptions={{headerShown: false, tabBarActiveTintColor: "#14B8A6", tabBarInactiveTintColor: "#6B7280"}}>
+            <Tab.Navigator initialRouteName="Test" screenOptions={{ tabBarStyle: {paddingBottom: 4},headerShown: false, tabBarActiveTintColor: "#14B8A6", tabBarInactiveTintColor: "#6B7280"}}>
                 <Tab.Screen name="Learn" component={Learn} options={{tabBarLabel: "Learn", tabBarIcon: ({color}) => <Feather name="book-open" size={24} color={color}/>}} />
                 <Tab.Screen name="Test" component={Test} options={{tabBarLabel: "Learn", tabBarIcon: ({color}) => <Foundation name="pencil" size={24} color={color}/>}} />
                 <Tab.Screen name="Profile" component={Profile} options={{tabBarLabel: "Profile", tabBarIcon: ({color}) => <Ionicons name="person" size={24} color={color}/>}}/>
