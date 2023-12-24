47ac23f234a65b4819e4837656da33a88a8d0302
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-12-24 11:14:39 +0200

    refactor: fix profile page

--- App.tsx
+++ App.tsx
@@ -27,7 +27,7 @@ export default function App() {
     <NavigationContainer>
       <SafeAreaView>
         <View className=" w-screen h-screen min-h-full flex pt-4">
-          <Quiz/>
+          <Main/>
         </View>
       </SafeAreaView>
     </NavigationContainer>
