dbeb2838e9d90e38e1588b67563746a298c54d3f
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-12-29 00:20:59 +0200

    feat: navigate to help and support page

--- src/pages/main/Profile.tsx
+++ src/pages/main/Profile.tsx
@@ -7,8 +7,12 @@ import {
 import { icons, images } from "../../../constants";
 import { Card } from "../../components/atoms/Card";
 import { FontAwesome, AntDesign, Feather } from "@expo/vector-icons"
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "src/router/stack";
 
 export const Profile = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
   return (
     <View className="w-screen h-screen  bg-gray-50">
       {/* topBar */}
@@ -30,7 +34,7 @@ export const Profile = () => {
           <Text className="text-base leading-6 font-normal text-gray-900">Payment and subscription</Text>
         </Card>
         {/* Help and support */}
-        <Card className="flex flex-row p-4 items-center space-x-3 self-stretch">
+        <Card className="flex flex-row p-4 items-center space-x-3 self-stretch" onPress={() => navigation.navigate('HelpSupport')}>
           <View className="flex p-2 bg-blue-200 rounded-full">
             <Feather name="help-circle" size={24} color={"#000000"}/>
           </View>
