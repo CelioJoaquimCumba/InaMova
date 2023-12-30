2870ed22122e342c2350e9318257daa8463e2239
Claudio Tomas <ccaducarlos@gmail.com>
2023-12-30 16:59:33 +0200

    feat: added link to GG in learn

--- App.tsx
+++ App.tsx
@@ -1,30 +1,28 @@
-import 'react-native-gesture-handler';
-import { SafeAreaView,Text,View } from 'react-native';
+import "react-native-gesture-handler";
+import { SafeAreaView, Text, View } from "react-native";
 import { NativeWindStyleSheet } from "nativewind";
-import { useColorScheme } from 'nativewind'
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { MainStack } from './src/router/stack';
-
+import { useColorScheme } from "nativewind";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { MainStack } from "./src/router/stack";
 
 
 NativeWindStyleSheet.setOutput({
-  default: "native",
+	default: "native",
 });
 
 export default function App() {
-  const { colorScheme, toggleColorScheme } = useColorScheme()
-  const handleClick = () => {
-    toggleColorScheme();
-  }
-  return (
-    <NavigationContainer>
-      {/* <SafeAreaView>
+	const { colorScheme, toggleColorScheme } = useColorScheme();
+	const handleClick = () => {
+		toggleColorScheme();
+	};
+	return (
+		<NavigationContainer>
+			{/* <SafeAreaView>
         <View className=" w-screen h-screen min-h-full flex pt-4"> */}
-          <MainStack/>
-        {/* </View>
+			<MainStack/>
+			{/* </View>
       </SafeAreaView> */}
-    </NavigationContainer>
-  );
+		</NavigationContainer>
+	);
 }
-
--- src/components/molecules/explore-card.tsx
+++ src/components/molecules/explore-card.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Image, ImageSourcePropType, Text, View } from "react-native";
-import { Feather } from "@expo/vector-icons"
+import { Feather } from "@expo/vector-icons";
 import { Button } from "../atoms/Button";
 import { Card } from "../atoms/Card";
 import { useNavigation } from "@react-navigation/native";
@@ -8,51 +8,61 @@ import { Learn, Quiz } from "../../../constants/paths";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "src/router/stack";
 import { ModalGetPremium } from "./modal-get-premium";
+import { Linking } from "react-native";
 
 interface TestExamProps {
-  id: string
-  image: any;
-  type: "test" | "learn"
-  title: string;
-  locked?: boolean;
-  onPress?: () => void;
+	id: string;
+	image: any;
+	type: "test" | "learn";
+	title: string;
+	locked?: boolean;
+	onPress?: () => void;
 }
 
 export const ExploreCard = ({
-  id,
-  image,
-  title,
-  type,
-  locked = false,
+	id,
+	image,
+	title,
+	type,
+	locked = false,
 }: TestExamProps) => {
-  const [modalVisible, setModalVisible] = useState(false);
-  const secondary = false
-  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
-  const handlePress = () => {
-    if(locked){
-      setModalVisible(true)
-      return
-    }
-    if(type === "test"){
-      navigation.navigate(Quiz)
-    }else {
-      navigation.navigate(Learn)
-    }
-  }
+	const [modalVisible, setModalVisible] = useState(false);
+	const secondary = false;
+	const navigation =
+		useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+	const handlePress = () => {
+		if (locked) {
+			setModalVisible(true);
+			return;
+		}
+		if (type === "test") {
+			navigation.navigate(Quiz);
+		} else {
+			// navigation.navigate(Learn)
+			Linking.openURL(
+				"https://drive.google.com/file/d/1AXf1HkcAUgD3C-34_5cxn00SttWxItFC/view"
+			).catch((error) => {
+				console.error("Error opening link:", error);
+			});
+		}
+	};
 
-  return (
-      <Card className=" p-4 space-y-2 shadow mx-2 border-b-2 border-gray-300">
-        <Image source={image} className={"h-36 aspect-square"} />
-        <Text className="text-base leading-6 font-bold">{title}</Text>
-        <Button
-          onPress={handlePress}
-          className="border border-gray-500 my-2 flex flex-row "
-          variant={"outline"}
-        >
-          {locked && <Feather name="lock" size={16} color="black" />}
-          <Text className="text-black ">Start</Text>
-        </Button>
-        <ModalGetPremium isVisible={modalVisible} toggleModal={() => setModalVisible(!modalVisible)}/>
-      </Card>
-  );
+	return (
+		<Card className=" p-4 space-y-2 shadow mx-2 border-b-2 border-gray-300">
+			<Image source={image} className={"h-36 aspect-square"} />
+			<Text className="text-base leading-6 font-bold">{title}</Text>
+			<Button
+				onPress={handlePress}
+				className="border border-gray-500 my-2 flex flex-row "
+				variant={"outline"}
+			>
+				{locked && <Feather name="lock" size={16} color="black" />}
+				<Text className="text-black ">Start</Text>
+			</Button>
+			<ModalGetPremium
+				isVisible={modalVisible}
+				toggleModal={() => setModalVisible(!modalVisible)}
+			/>
+		</Card>
+	);
 };
