ac37e9b217203c11344aa1b76cf07673547f1945
Claudio Tomas <ccaducarlos@gmail.com>
2024-03-25 20:50:51 +0200

    test

--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -82,6 +82,7 @@ export const ArticlePage = ({ route }: Props) => {
 					{/* <Image source={images.CarDriving} className='w-42'/> */}
 					<Text className="text-xl leading-7 font-semibold">
 						{lesson.title}
+						{/* aaa */}
 					</Text>
 					{bodyText[0]}
 					<View className=" flex flex-row justify-between">
